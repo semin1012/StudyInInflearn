d119d0f3686c9e3288e251c14b9fe7ea29cc41a2
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-03-02 23:15:15 +0900

    변수

--- GameCoding/GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding/GameCoding.cpp
@@ -1,8 +1,15 @@
 ﻿#include <iostream>
+using namespace std;
 
 int hp;
+int maxHp;
+// 임대하는 것처럼 공간을 확보받았다는 것이 중요 포인트
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	hp = 0x1100;
+	// 0b를 붙이면 2진법
+	// ox를 붙이면 16진법
+	 
+	cout << hp << endl;
 }
