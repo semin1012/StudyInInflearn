fbad84a29d28c7be8e5a01f92c3ad73490c8be25
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-03-11 17:48:22 +0900

    3주차 달팽이 문제

--- GameCoding/GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding/GameCoding.cpp
@@ -1,67 +1,120 @@
 ﻿#include <iostream>
 using namespace std;
+#include <iomanip>
 
-// 로또 번호 생성기
-void Swap(int& a, int& b)
-{
-	int temp = a;
-	a = b;
-	b = temp;
-}
+const int MAX = 100;
+int board[MAX][MAX];
+int N;
 
-// { 1, 42, 3, 15, 5, 6 } -> { 1, 3, 5, 6, 15, 42 }
-void Sort(int* numbers, int count)
+void PrintBoard()
 {
-	for (int i = 0; i < count; i++)
-	{
-		// 마지막 숫자는 하면 안 되니까 -1
-		// i번째 가장 큰 숫자는 이미 뒤로 가 있으니까 -i
-		for (int j = 0; j < count - 1 - i; j++)
-		{
-			if (numbers[j] > numbers[j + 1])
-				Swap(numbers[j], numbers[j + 1]);
+	for (int x = 0; x < N; x++) {
+		for (int y = 0; y < N; y++) {
+			cout << setfill('0') << setw(2) << board[y][x] << " ";
 		}
+		cout << endl;
 	}
 }
 
-void ChooseLotto(int* numbers)
+enum DIR
 {
-	int count = 0;
+	RIGHT = 0,
+	DOWN = 1,
+	LEFT = 2,
+	UP = 3,
+};
 
-	while (count != 6)
+bool CanGo(int x, int y)
+{
+	if (y < 0 || y >= N)
+		return false;
+	if (x < 0 || x >= N)
+		return false;
+	if (board[y][x] != 0)
+		return false;
+	return true;
+}
+
+void SetBoard()
+{
+	int dir = RIGHT;
+	int num = 1;
+	int y = 0;
+	int x = 0;
+
+	// 방향을 enum 대신 간단하게 쓰고 싶을 때
+	int dy[] = { 0, 1, 0, -1 };
+	int dx[] = { 1, 0, -1, 0 };
+
+	while (true)
 	{
-		// 1~45 랜덤 추출
-		int randValue = 1 + rand() % 45; 
+		board[x][y] = num;
+
+		if (num == N * N)
+			break; // 끝 숫자에 도달하면 break
+
 		
-		// 이미 찾은 값인지?
-		bool found = false;
-		for (int i = 0; i < count; i++) {
-			if (numbers[i] == randValue) {
-				found = true;
-				break;
-			}
-		}
+		int nextY = y + dy[dir];
+		int nextX = x + dx[dir];
 
-		if (found == false)
+		/*int nextY;
+		int nextX;
+
+		switch (dir)
 		{
-			numbers[count] = randValue;
-			count++;
-		}
+		case RIGHT:
+			nextY = y;
+			nextX = x + 1;
+			break;
+		case DOWN:
+			nextY = y + 1;
+			nextX = x;
+			break;
+		case LEFT:
+			nextY = y;
+			nextX = x - 1;
+			break;
+		case UP:
+			nextY = y - 1;
+			nextX = x;
+			break;
+		}*/
 
-		numbers[count] = randValue;
+		if (CanGo(nextY, nextX)) 
+		{
+			y = nextY;
+			x = nextX;
+			num++;
+		}
+		else  
+		{
+			// 방향을 enum 대신 쓰고 싶을 때
+			dir = (dir + 1) % 4;
+
+			/*switch (dir)
+			{
+			case RIGHT:
+				dir = DOWN;
+				break;
+			case DOWN:
+				dir = LEFT;
+				break;
+			case LEFT:
+				dir = UP;
+				break;
+			case UP:
+				dir = RIGHT;
+				break;
+			}*/
+		}
 	}
 
-	Sort(numbers, 6);
+	PrintBoard();
 }
 
 int main()
 {
-	srand((unsigned)time(0));
-	int lotto[6];
-	ChooseLotto(lotto);
+	cin >> N;
 
-	Sort(lotto, 6);
-
-	for (int i = 0; i < 6; i++)
-		cout << lotto[i] << " ";
+	SetBoard();
 }
