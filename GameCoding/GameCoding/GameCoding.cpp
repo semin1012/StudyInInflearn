a04115ca25200aa804d62af012e585d5615a1a90
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-03-12 18:24:42 +0900

    객체지향 클래스 생성

--- GameCoding/GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding/GameCoding.cpp
@@ -2,119 +2,76 @@
 using namespace std;
 #include <iomanip>
 
-const int MAX = 100;
-int board[MAX][MAX];
-int N;
+// 객체지향(Object Oriented Programming)
+// 데이터 + 로직
 
-void PrintBoard()
+class Knight
 {
-	for (int x = 0; x < N; x++) {
-		for (int y = 0; y < N; y++) {
-			cout << setfill('0') << setw(2) << board[y][x] << " ";
-		}
-		cout << endl;
+public:
+	// 기본 생성자(constructor)
+	// 생성자에 밑줄이 뜨는 이유: 멤버 변수 초기화하라고
+	// 초기화하지 않으면 쓰레기값이 들어가게 됨, 예상 불가 
+	Knight()
+	{
+		m_hp = 0;
+		m_attack = 0;
+		m_defence = 0;
+		cout << "Knight()" << endl;
 	}
-}
 
-enum DIR
-{
-	RIGHT = 0,
-	DOWN = 1,
-	LEFT = 2,
-	UP = 3,
+	// 기타 생성자
+	// 만약 기본 생성자 없이 기타 생성자만 존재하다면 컴파일러가 기본 생성자를 안 처리해줌
+	// 꼭 인자를 넣는 방식의 기타 생성자로만 Knight를 생성할 수 있게 됨
+	Knight(int hp, int attack, int defence)
+	{
+		m_hp = hp;
+		m_attack = attack;
+		this->m_defence = defence;
+		// 인자와 멤버변수 이름이 같은 경우 this로 명시 가능
+		cout << "Knight" << endl;
+	}
+	
+	// 복사 생성자
+	// 두개의 같은 타입 객체를 복사해서
+	// 동일한 상태의 데이터를 갖는 다른 객체를 만든다
+	Knight(const Knight& other)
+	{
+		m_hp = other.m_hp;
+		m_attack = other.m_attack;
+		m_defence = other.m_defence;
+	}
+
+	// 소멸자(destructor)
+	~Knight()
+	{
+		cout << "~Knight()" << endl;
+	}
+
+	// 멤버 함수
+	void Attack() { cout << "Attack " << endl; }
+	void Die() { cout << "Die" << endl; }
+	void HealMe(int value) 
+	{
+		m_hp += value;
+	}
+
+public:
+	// 멤버 변수
+	int m_hp;
+	int m_attack;
+	int m_defence;
 };
 
-bool CanGo(int x, int y)
+int main() 
 {
-	if (y < 0 || y >= N)
-		return false;
-	if (x < 0 || x >= N)
-		return false;
-	if (board[y][x] != 0)
-		return false;
-	return true;
-}
-
-void SetBoard()
-{
-	int dir = RIGHT;
-	int num = 1;
-	int y = 0;
-	int x = 0;
-
-	// 방향을 enum 대신 간단하게 쓰고 싶을 때
-	int dy[] = { 0, 1, 0, -1 };
-	int dx[] = { 1, 0, -1, 0 };
-
-	while (true)
-	{
-		board[x][y] = num;
-
-		if (num == N * N)
-			break; // 끝 숫자에 도달하면 break
-
-		
-		int nextY = y + dy[dir];
-		int nextX = x + dx[dir];
-
-		/*int nextY;
-		int nextX;
-
-		switch (dir)
-		{
-		case RIGHT:
-			nextY = y;
-			nextX = x + 1;
-			break;
-		case DOWN:
-			nextY = y + 1;
-			nextX = x;
-			break;
-		case LEFT:
-			nextY = y;
-			nextX = x - 1;
-			break;
-		case UP:
-			nextY = y - 1;
-			nextX = x;
-			break;
-		}*/
-
-		if (CanGo(nextY, nextX)) 
-		{
-			y = nextY;
-			x = nextX;
-			num++;
-		}
-		else  
-		{
-			// 방향을 enum 대신 쓰고 싶을 때
-			dir = (dir + 1) % 4;
-
-			/*switch (dir)
-			{
-			case RIGHT:
-				dir = DOWN;
-				break;
-			case DOWN:
-				dir = LEFT;
-				break;
-			case LEFT:
-				dir = UP;
-				break;
-			case UP:
-				dir = RIGHT;
-				break;
-			}*/
-		}
-	}
-
-	PrintBoard();
-}
-
-int main()
-{
-	cin >> N;
-
-	SetBoard();
+	// 객체 (instance)
+	Knight k1(100, 10, 1);
+	//Knight k1 = Knight(100, 10, 1);
+	//Knight k1{ 100, 10, 1 };
+	//Knight k1 = { 100, 10, 1 };
+	//모두 같은 코드임, 문법만 다르다
+	
+	//복사 생성자 사용법은 밑에처럼 쓴다
+	Knight k2(k1);
+	k1.Attack();
 }
--- GameCoding/GameCoding/Player.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void HandleMove();
-void MovePlayer(int x, int y);
-
-extern int GPlayerX;
-extern int GPlayerY;
