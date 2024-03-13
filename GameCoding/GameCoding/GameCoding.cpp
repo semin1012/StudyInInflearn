6ea1190a3b0105e3ef2c4066a63e9ab9a5afb343
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-03-13 21:13:40 +0900

    4주차 상속성 진행 중

--- GameCoding/GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding/GameCoding.cpp
@@ -5,73 +5,53 @@ using namespace std;
 // 객체지향(Object Oriented Programming)
 // 데이터 + 로직
 
-class Knight
+// OOP 3대 요소
+// - 상속성 << inheritance
+//	 물려받는 것. 부모 클래스에서 물려받을 수 있는 것.
+// - 은닉성
+// - 다형성
+
+class Player
 {
 public:
-	// 기본 생성자(constructor)
-	// 생성자에 밑줄이 뜨는 이유: 멤버 변수 초기화하라고
-	// 초기화하지 않으면 쓰레기값이 들어가게 됨, 예상 불가 
-	Knight()
-	{
-		m_hp = 0;
-		m_attack = 0;
-		m_defence = 0;
-		cout << "Knight()" << endl;
-	}
-
-	// 기타 생성자
-	// 만약 기본 생성자 없이 기타 생성자만 존재하다면 컴파일러가 기본 생성자를 안 처리해줌
-	// 꼭 인자를 넣는 방식의 기타 생성자로만 Knight를 생성할 수 있게 됨
-	Knight(int hp, int attack, int defence)
-	{
-		m_hp = hp;
-		m_attack = attack;
-		this->m_defence = defence;
-		// 인자와 멤버변수 이름이 같은 경우 this로 명시 가능
-		cout << "Knight" << endl;
-	}
-	
-	// 복사 생성자
-	// 두개의 같은 타입 객체를 복사해서
-	// 동일한 상태의 데이터를 갖는 다른 객체를 만든다
-	Knight(const Knight& other)
-	{
-		m_hp = other.m_hp;
-		m_attack = other.m_attack;
-		m_defence = other.m_defence;
-	}
-
-	// 소멸자(destructor)
-	~Knight()
-	{
-		cout << "~Knight()" << endl;
-	}
-
-	// 멤버 함수
-	void Attack() { cout << "Attack " << endl; }
-	void Die() { cout << "Die" << endl; }
-	void HealMe(int value) 
-	{
-		m_hp += value;
-	}
+	void Move() {}
+	void Attack() { }
+	void Die() { }
 
 public:
-	// 멤버 변수
-	int m_hp;
-	int m_attack;
-	int m_defence;
+	int _hp;
+	int _attack;
+	int _defence;
 };
 
-int main() 
+//설계도
+class Knight : public Player
 {
-	// 객체 (instance)
-	Knight k1(100, 10, 1);
-	//Knight k1 = Knight(100, 10, 1);
-	//Knight k1{ 100, 10, 1 };
-	//Knight k1 = { 100, 10, 1 };
-	//모두 같은 코드임, 문법만 다르다
-	
-	//복사 생성자 사용법은 밑에처럼 쓴다
-	Knight k2(k1);
-	k1.Attack();
+public:
+
+public:
+};
+
+class Archer : public Player
+{
+public:
+
+public:
+	int _arrowCount;
+};
+
+class Mage : public Player
+{
+public:
+
+public:
+	int _mp;
+};
+
+int main()
+{
+	Knight k1;
+	k1._hp;
+	Mage m1;
+	m1._hp;
 }
