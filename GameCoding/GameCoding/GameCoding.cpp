ae1ebdf83293e015a4856a03e0dce3782df14848
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-03-22 15:00:59 +0900

    4주차 연산자 오버로딩
    https://metal-catamaran-8b6.notion.site/6-5a8a1f04a9b944519dc0fa2c20bc2e11?pvs=4

--- GameCoding/GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding/GameCoding.cpp
@@ -5,91 +5,107 @@ using namespace std;
 // 객체지향(Object Oriented Programming)
 // 데이터 + 로직
 
-class Player
+// 연산자 오버로딩
+// - 멤버 연산자 함수 버전
+//	 a op b 형태에서 왼쪽을 기준으로 실행된다.
+// - 전역 연산자 함수 버전
+//	 a op b 형태라면, a/b 모두를 연산자 함수의 피연산자로 사용한다.
+
+class Pos
 {
 public:
-	Player()
+	Pos() { }
+
+	// 간접적으로 변환되어 넘어오는 것을 예방하는 것이다
+	// ex) TestPos(10)이 가능하게 되는 것을 예방하는 것
+	explicit Pos(int b) 
 	{
-		cout << "Player() " << endl;
+		x = b;
+		y = b;
 	}
 
-	~Player()
+	// 멤버 연산자 함수 버전
+	Pos operator+ (const Pos& b)
 	{
-		cout << "~Player()" << endl;
+		Pos pos;
+		pos.x = x + b.x;
+		pos.y = y + b.y;
+		return pos;
 	}
+
+	Pos operator+(int b)
+	{
+		Pos pos;
+		pos.x = x + b;
+		pos.y = y + b;
+		return pos;
+	}
+
+	Pos(int a, int b)
+	{
+		x = a;
+		y = b;
+	}
+
+	bool operator==(const Pos& b)
+	{
+		return x == b.x && y == b.y;
+	}
+
+	// 대입 연산자
+	void operator=(int b)
+	{
+		x = b;
+		y = b;
+	}
+
+	//Pos& operator=(int b)
+	//{
+	//	x = b;
+	//	y = b;
+	//	return *this;
+	//}
+	// 이걸 쓰면 pos1 = pos2 = pos3; 같은 코드를 사용 가능
+
+	int x = 0;
+	int y = 0;
 };
 
-class Inventory
+Pos operator+(int a, const Pos& b)
 {
-public:
-	Inventory() : _a(0)
-	{
-		cout << "Inventory()" << endl;
-	}
+	Pos pos;
+	pos.x = a + b.x;
+	pos.y = a + b.y;
+	return pos;
+}
 
-	Inventory(int a)
-	{
-		_a = a;
-		cout << "Inventory(int)" << endl;
-	}
-
-	~Inventory()
-	{
-		cout << "~Inventory()" << endl;
-	}
-
-public:
-	int _a;
-};
-
-class Knight : public Player
+void TestPos(Pos a)	// TestPos(10) 이라고 해도 됨
 {
-public:
-	Knight() : _hp(0), _inventory(Inventory(100)), _hpRef(_hp), _hpConst(_hp)
-		/*
-			선처리 영역
-			Player()
-			_inventory = Inventory(100)
-		*/
-	{
-		// 생성자에서 변수들 초기화하는 것이 좋다
-		// _inventory = Inventory(100); // Inventory(), Inventory(int) 호출 두 번 됨
-		// 그래서 위의 코드 말고 Knight() : _hp(0)처럼 옆으로 쓰면 한 번만 호출된다. 이게 깔끔하다.
-		cout << "Knight()" << endl;
-	}
-
-	Knight(int hp) : _hp(hp)
-	{
-		cout << "Knight(int)" << endl;
-	}
-
-	~Knight()
-	{
-		cout << "~Knight()" << endl;
-	}
-
-public:
-	int _hp;
-	int _defence = 20;
-
-	// 밑에 둘은 Knight() : 이 뒤에서만 초기화 가능하다
-	// const는 처음 세팅값이 불변, 래퍼런스는 무언가를 가르키고 있어야 하기 때문
-	int& _hpRef;
-	const int _hpConst;
-
-	Inventory _inventory;
-};
-
+	// TestPos(10)이 되는 이유는?
+	// Pos = 10 이 만들어질 수 있는 대입 연산자가 존재하기 때문에
+	// TestPos(10)도 만들어질 수가 있다는 포인트 
+}
 
 int main()
 {
-	// C# Knight k1 = new Knight();
+	int a = 10;
+	int b = 20;
+	int c = a + b;
 
-	// C++ Knight* k1 = new Knight();
+	Pos pos1 = Pos(10, 20);
+	Pos pos2 = Pos(40, 20);
 
-	Knight k1;
-	// 부모 자식의 호출 순서 주의!
-	// Player() -> Knight() -> ~Knight() -> ~Player()
+	Pos pos3 = pos1 + pos2;
 
+	Pos pos4 = 10 + pos2;
 
+	// 밑에 두 방법 모두 생성자(int)가 불린다
+	Pos pos5(10);
+	Pos pos6 = 10;
+
+	// 대입, 생성이 된 이후에 호출되는 게 대입
+	pos5 = 10;
+	pos6 = 20;
+
+	cout << pos3.x << " " << pos3.y << endl;
 }
