486aaa971d01175d2a89ee9faaa329277bbeb73d
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-03-22 15:38:30 +0900

    4주차 static 개념

--- GameCoding/GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding/GameCoding.cpp
@@ -5,27 +5,62 @@ using namespace std;
 // 객체지향(Object Oriented Programming)
 // 데이터 + 로직
 
-// struct vs class
-//  - 차이가 거의 없음
-//	- struct는 public이 기본
-//	- class는 private가 기본
+// stati : 특정 객체에 종속적인지
+//	 - 클래스로 만들어낸 모든 객체의 값이 공통되게 된다.
+//	 - 메모리 낭비가 되지 않도록 함.
 
-struct Knight
+// 자료구조&알고리즘
+// 디자인 패턴
+//  - 싱글톤, 옵저버, 콤포넌트, ...
+
+// 싱글톤 - 정말 딱 1개만 존재하고 어디서든 사용할 수 있는 [매니저] 클래스
+
+class UserManager
 {
-	int hp;
+public:
+	static UserManager* GetInstance()
+	{
+		// um은 static이라 다른 메모리에 만들어지게 됨, 사라지지 않음
+		static UserManager um;
+		return &um;
+	}
+
+public:
+	void AddUser() { _userCount++; }
+	int GetUserCount() { return _userCount; }
+
+private:
+	int _userCount = 0;
 };
 
-class Knight2
+class Player
 {
-	int hp;
+public:
+	Player()
+	{
+		id = s_idGenerator++;
+	}
+
+public:
+	int id;
+	// 처음 만들 때 초기화해 줘야 되겠지만 이렇게 id를 만들면 편해서 이렇게 잘 쓴다
+	static int s_idGenerator;
 };
+
+int Player::s_idGenerator = 1;
+
+int GenerateId()
+{
+	static int s_id = 10; // 안 사라짐
+	return s_id++;
+}
+
+#define GET_MANAGER (UserManager::GetInstance())
 
 int main()
 {
-	Knight k1;
-	k1.hp = 10;
+	for (int i = 0; i < 10; i++)
+		GET_MANAGER->AddUser(); // = UserManager::GetInstance()->AddUser();
 
-	// Knight2 k2;
-	// k2.hp = 20;
-	// 클래스는 hp가 기본적으로 private라서 안 됨
+	cout << GET_MANAGER->GetUserCount() << endl;
 }
