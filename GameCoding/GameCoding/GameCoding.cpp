1d3637b88e4f89dc672d89472acffc90c55dbe49
semin1012 <semin1012@gmail.com>
2024-03-27 16:17:48 +0900

    6주차 연결 리스트
    https://metal-catamaran-8b6.notion.site/8-17444585b8fc48b8a1529627e3975263?pvs=4

--- GameCoding/GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding/GameCoding.cpp
@@ -1,73 +1,43 @@
 ﻿#include <iostream>
 using namespace std;
-#include <iomanip>
 
-// MMO 동접 1만, 몬스터 50만
-
-// - 스택
-// - 메모리
-// 두 가지로는 방대한 MMO RPG 데이터를 감당하기 힘들다
-// 스택은 공간 자체가 좁아서 백만마리 몬스터 생성부터 불가능
-// 데이터(전역)은 만들 수는 있지만 공간 해제할 수가 없음
-
-class Monster
+class Node
 {
 public:
-	Monster() { cout << "Monster()" << endl; }
-	~Monster() { cout << "~Monster()" << endl; }
+	Node(int data) : data(data) { }
 
 public:
-	int _hp = 0;
+	int data; 
+	// 포인터를 안 붙이면 러시아 인형처럼 끝없이 자기 자신을 타고 들어가서 끝 안 나는 클래스가 됨
+	// 포인터를 붙이면 그저 주소값을 넣는 것 
+	Node* prev = nullptr;
+	Node* next = nullptr;
 };
-
-class Player
-{
-public:
-	Monster* _target;
-};
-
-// 스택은 불안정한 공간이지만 힙은 안정적인 공간이다
-// 다만 힙은 직접적으로 관리해야 한다는 단점이 있다
 
 int main()
 {
-	// malloc + free 세트
-	// new + delete 세트
-	// 위 둘의 차이는 연산자 차이다. 생성자, 소멸자의 호출에 차이가 있음.
-	//  - malloc은 생성자, 소멸자가 전혀 안 뜸. 
-
+	// 양방향이 더 장점이 많고 더 많이 쓰인다. 
+	// 새 정보를 넣고 싶을 때 앞뒤 정보를 같이 비교해서 넣는 게 일반적이기 때문에.
+	Node* n1 = new Node(1);
+	Node* n2 = new Node(2);
+	Node* n3 = new Node(3);
+	Node* n4 = new Node(4);
+	Node* n5 = new Node(5);
 	
-	// malloc 사용법, 생성자 호출 X, 공간을 지정해야 하는 불편함
-	/*void* ptr = malloc(1000); // heap 영역에 할당해 달라고 하는 것
+	// [1]<->[2]<->[3]<->[4]<->[5]
+	// [head]				   [tail]
+	n1->next = n2;
+	n2->prev = n1;
 	
-	Monster* m = (Monster*)ptr;
-	m->_hp = 100;
-	
-	free(ptr);*/ // 해제를 수기로 해야 함, 안 하면 메모리 누수
+	n2->next = n3;
+	n3->prev = n2;
 
+	n3->next = n4;
+	n4->prev = n3;
 
-	// new 사용법, 생성자 호출됨
-	// 연달아 있는 배열로 호출 가능, 지울 때도 delete[] 해야 함
-	Monster* m1 = new Monster();
-	//Monster* m1 = new Monster[5]();
-	m1->_hp = 100;
+	n4->next = n5;
+	n5->prev = n4;
 
-	delete m1;
-
-	// 이거 의외로 크래시 안 남
-	m1->_hp = 200;
-	// 매번 요청하는 대로 생성,삭제하면 너무 느림
-	// 그래서 일단 날리지는 않고 들고 있는다
-	// 잡을 수 없는 문제가 되면 더욱 큰일이 될 수 있음
-
-	m1 = nullptr;
-	// = 0이라고 명시해도 됨
-	// 이렇게 바뀌면 사용하려고 할 때 크래쉬가 난다.
-
-	Player* player = new Player();
-	player->_target = m1;
-
-	delete m1;
-	m1 = nullptr;
-	// 이렇게 해도 player->_taget은 사라지지 않음. 크래쉬 가능성 큼.
+	Node* head = n1;
+	Node* tail = n5;
 }
