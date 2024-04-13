f507de6c7c6fe3f9cd813f178fa4436745e8be5e
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-04-13 18:37:55 +0900

    5주차 타입 캐스팅 4가지

--- GameCoding/GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding/GameCoding.cpp
@@ -1,58 +1,78 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 얕은 복사 vs 깊은 복사
-class Pet
+// 캐스팅 4총사
+// static_cast <<
+// dynamic_cast <<
+// const_cast
+// reinterpret_cast
+
+// 맨 위의 두 가지만 자주 쓰지만 면접에서는 다 잘 나온다. c++에서 굉장히 중요한 문제.
+
+class Player
 {
 public:
-	Pet() { cout << "Pet()" << endl; }
-	~Pet() { cout << "~Pet()" << endl; }
-
-	Pet(const Pet& pet) { cout << "Pet(const Pet&)" << endl; }
+	virtual ~Player();
 };
 
-class Knight
+class Knight : public Player
 {
-public:
-	Knight()
-	{
-		_pet = new Pet();
-	}
 
-	~Knight()
-	{
-		delete _pet;
-	}
+};
 
-	Knight(const Knight& k)	// 복사 생성자
-	{
-		_hp = k._hp;
-		_pet = new Pet(*(k._pet)); // k.pet 의 주소에 이동해서 참조 값으로 넘어갈 수 있게 된다.
-	}
+class Archer : public Player
+{
 
-	void operator=(const Knight& k) // 복사 연산자
-	{
-		_hp = k._hp;
-	}
+};
 
-public:
-	int _hp = 100;
-	Pet* _pet;
+class Dog
+{
+
 };
 
 int main()
 {
-	Knight k1;
-	k1._hp = 200;
+	// static_cast : 타입 원칙에 비춰볼 때 상식적인 캐스팅만 허용해 준다.
+	// 1) int <-> float 변환, 정수와 부동소수점 사이의 변화는 당연히 가능하다.
 
-	Knight k2 = k1;	// 복사 생성자, 얕은 복사가 된다.
-	// 값을 하나하나 일일이 똑같이 넣어 주는 게 얕은 복사다.
-	// 같은 주소를 가르키고 있는데 이중으로 delete를 할 수 있어서 프로그램이 뻗어버릴 수 있다.
+	int hp = 100;
+	int maxHp = 200;
+	//float ratio = hp / maxHp;	// 이렇게만 하면 0.5가 나와야 하는데 정수를 정수로 나누어서 0이 나옴.
+	float ratio = static_cast<float>(hp) / maxHp;	// float이랑 정수를 나누어야 실수가 결과로 나온다.
 
-	// 깊은 복사란 포인터를 타고 간 메모리에 있는 정보를 또 다른 메모리에 복사하는 것이다.
-	// 같은 메모리를 가르키는 게 아니라, 다른 메모리를 가르키는데 내용은 똑같이 복사하는 것.
+	Knight* k = new Knight();
+	Player* p = k;				// 기사는 플레이어입니까? 당연히 Yes. 당연히 타입 변환을 해 준다.
+
+	//Knight k2 = p;			// 플레이어는 기사입니까? 이건 고민해볼 문제. 안 해 준다. 
+	//Knight* k2 = (Knight*)p;	// 상식적이지는 않지만 가능하다. 통과시켜 줌. c 스타일 문법이다.
+	//Knight* k2 = static_cast<Knight*>(p);	// 얘도 통과시켜 줌. c++ 스타일 문법이다.
+	// 만약 원본인 k가 Archer 였다면 난리 남. Archer가 Knight로 변화된 것.
 	
-	//Knight k3;		// 기본 생성자
-	//k3 = k1;		// 복사 연산자
+	// dynamic_cast : 상속 관계에서의 안전한 변환을 보장한다.
+	// 다형성을 활용하는 방식 (virtual)
+	// RTTI (RunTime Type Information)
+	// Player에 virtual 함수가 하나라도 있으면 가상 함수 테이블이 메모리에 같이 만들어진다.
+	// 즉, 그래서 원본 데이터가 Knight 타입이 맞는지 확인 가능. 
+	// 만약 이때 원본이 Archer 라면 0(nullptr)으로 밀어 준다. 타입이 맞을 때만 값을 넣어 준다.
+	Knight* k2 = dynamic_cast<Knight*>(p);
+	// nullptr이 아니라면
+	if (k2 != nullptr)
+	{
+		// 기사가 맞다는 것 확신 가능. 이런 식으로 if 문이랑 같이 씀.
+	}
+
+	//const_cast : const와 관련된 cast를 할 수가 있다. 실상에서 진짜 안 씀. 
+	const char* name = "Semin";
+	char* name2 = const_cast<char*>(name);	// const를 잠시 뗄 수가 있다.
+
+	// reinterpret_cast
+	// 위험하고, 강력한 형태
+	// re-interpret : 다시 생각하다?
+	// 포인터 -> 전혀 관계없는 다른 포인터로 변환한다.
+	Dog* dog = reinterpret_cast<Dog*>(k);	// 상식적인 캐스팅이 아니다.
+	// malloc으로 메모리 할당하고 그걸 Dog*에 할당하고 싶은 경우나
+	// 포인터 값을 잠시 정수로 들고 있고 싶을 때 사용할 수 있다.
+	// 상속 관계도 전혀 없고, 관련이 전혀 없는 클래스 둘을 '사용 용도가 확실'할 때 쓸 수 있도록 한 캐스팅이다.
+	// 얘도 물론 거의 안 쓴다.
 }
  
