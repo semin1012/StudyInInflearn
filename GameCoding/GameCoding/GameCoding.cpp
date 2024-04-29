61e3baf40a241b661f8238a9ea1af267f77609ca
semin1012 <semin1012@gmail.com>
2024-04-29 21:42:41 +0900

    7주차 PrioityQueue 힙 트리
     수정 필요

--- GameCoding/GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding/GameCoding.cpp
@@ -1,112 +1,108 @@
 ﻿#include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
-class Node
+template<typename T>
+class PrioityQueue
 {
 public:
-	Node(const char* data) : data(data) { }
+	void push(bool T& data)
+	{
+		// 우선 힙 구조부터 맞춰 준다.
+		_heap.push_back(data);
 
-public:
-	const char* data;
-	vector<Node*> childen;
+		// 도장 깨기 시작, 맨 뒤에서부터 시작
+		int now = static_cast<int>(_heap.size()) - 1;
+
+		// 루트 노드까지
+		while ( now > 0 ) 
+		{
+			int next = (now - 1) / 2; // 부모 인덱스 공식
+			if (_heap[now] < _heap[next])
+				break;	// 반란 실패하면 break로 빠져나오기
+
+			// 반란 성공하면 데이터 교체
+			::swap(_heap[now], _heap[next]);
+			now = next;
+		}
+	}
+
+	void pop()
+	{
+		_heap[0] = _heap.back(); // _heap.back() < 마지막 데이터를 의미
+		_heap.pop_back(); // 데이터를 첫 번째로 옮기고, 마지막 자리는 삭제해 줌
+
+		int now = 0;
+
+		while(true)
+		{
+			int left = 2 * now + 1;
+			int right = 2 * now + 2;
+
+			// 리프에 도달할 경우 (더 내려갈 곳 없으면) break
+			if ( left >= (int)_heap.size() )
+				break;
+			
+			int next = now;
+
+			// 왼쪽 비교
+			if ( _heap[next] < _heap[left] )
+				next = left;
+			
+			// 둘 중 승자를 오른쪽과 비교
+			if ( right < (int)_heap.size() && _heap[next] < _heap[right] )
+				next = right;
+
+			// 왼쪽/오른쪽 둘 다 현재 값보다 작으면 종료
+			if ( next == now )
+				break;
+			
+			::swap(_heap[now], _heap[next]);
+			now = next;
+		}
+	}
+
+	T& top()
+	{
+		return _heap[0];
+	}
+
+	bool empty()
+	{
+		return _heap.empty();
+	}
+
+	void PrintTree()
+	{
+	}
+
+private:
+	vector<T> _heap;	// 데이터를 넣어 줄 곳
+	
 };
-
-Node* CreateTree()
-{
-	Node* root = new Node("R1 개발실");
-	{
-		Node* node = new Node("디자인팀");	
-		root->childen.push_back(node);
-		{
-			Node* leaf = new Node("전투");
-			node->childen.push_back(leaf);
-		}
-		{
-			Node* leaf = new Node("경제");
-			node->childen.push_back(leaf);
-		}
-		{
-			Node* leaf = new Node("스토리");
-			node->childen.push_back(leaf);
-		}
-	}
-	{
-		Node* node = new Node("프로그래밍팀");	
-		root->childen.push_back(node);
-		{
-			Node* leaf = new Node("클라");
-			node->childen.push_back(leaf);
-		}
-		{
-			Node* leaf = new Node("서버");
-			node->childen.push_back(leaf);
-		}
-		{
-			Node* leaf = new Node("엔진");
-			node->childen.push_back(leaf);
-		}
-	}
-	{
-		Node* node = new Node("아트팀");	
-		root->childen.push_back(node);
-		{
-			Node* leaf = new Node("배경");
-			node->childen.push_back(leaf);
-		}
-		{
-			Node* leaf = new Node("캐릭터");
-			node->childen.push_back(leaf);
-		}
-	}
-
-	return root;
-}
-
-// 깊이(Depth): 루트에서 어떤 노드에 도달하기 위해 거쳐야 하는 간선의 개수
-void PrintTree(Node* root, int depth = 0)
-{
-	for( int i = 0 ; i < depth; i++ )
-		cout << " ";
-
-	cout << root->data << endl;
-
-	int size = root->childen.size();
-	if ( size > 0 )
-	{
-		for ( int i = 0 ; i < size; i++ ) 
-		{
-			Node* node = root->childen[i];
-			PrintTree(node, depth+1);
-		}
-		cout << endl;
-	}	
-}
-
-// 높이 
-int GetHeight(Node* root)
-{
-	int ret = 1;
-
-	int size = root->childen.size();
-	for ( int i = 0 ; i < size ; i++ )
-	{
-		Node* node = root->childen[i];
-		int h = GetHeight(node) + 1;
-		// 이게 이해 안 가면 노션 
-
-		if (ret < h)
-			ret = h;
-	}
-
-	return ret;
-}
 
 int main()
 {
-	Node* root = CreateTree();
+	vector<int> v;
+	PrioityQueue<int> pq;
 
-	PrintTree(root);
+	pq.push(10);
+	pq.push(40);
+	pq.push(30);
+	pq.push(50);
+	pq.push(20);
 
-	cout << GetHeight(root) << endl;
-}
+	pq.PrintTree();
+
+	int value = pq.top();
+	cout << pq.top() << endl;
+
+	// 가장 큰값을 찾아내기 위한 효율적인 방법은?
+
+	// 정렬 - 빠른 퀵 소트도 O(NlogN)
+	// 가장 큰 값 스캔 - O(N), 이게 더 낫다
+
+	// [][][][][][][][][][]
+
+} 
