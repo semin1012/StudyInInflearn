2cc435e3405658ef9c3d8d11b3979b20784ecf55
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-05-13 21:10:48 +0900

    Mac에서 공부한 내용 가지고 오기 (DFS, BFS)

--- GameCoding/Maze/Player.cpp
+++ GameCoding/Maze/Player.cpp
@@ -7,8 +7,8 @@ void Player::Init(Board* board)
 	_pos = board->GetEnterPos();
 	_board = board;
 
-	//CalculatePath_RightHand();
-	CalculatePath_BFS();
+	CalculatePath_RightHand();
+	//CalculatePath_BFS();
 }
 
 void Player::Update(uint64 deltaTick)
@@ -42,12 +42,6 @@ void Player::CalculatePath_RightHand()
 	// 목적지
 	Pos dest = _board->GetExitPos();
 
-	//for (int i = 0; i < 20; i++)
-	//{
-	//	pos += Pos(1, 0);
-	//	_path.push_back(pos);
-	//}
-
 	_dir;	// 내가 바라보는 방향
 	pos;	// 현재 좌표
 
@@ -60,7 +54,7 @@ void Player::CalculatePath_RightHand()
 		Pos(0, 1),	//RIGHT
 	};
 
-	Pos next = pos + front[_dir];
+	//Pos next = pos + front[_dir];
 
 	/*switch (_dir)
 	{
@@ -80,41 +74,42 @@ void Player::CalculatePath_RightHand()
 
 	// 오른쪽 방향 90도 회전
 	// DIR_LEFT 순서보다 빠르게 DIR_UP이 있기 때문에 -1
-	_dir = (_dir - 1) % DIR_COUNT;
+	//_dir = (_dir - 1) % DIR_COUNT;
 	// 왼쪽 방향 90도 회전
-	_dir = (_dir + 1) % DIR_COUNT;
+	//_dir = (_dir + 1) % DIR_COUNT;
 
 	// 목적지 찾을 때까지
-	//while (pos != dest)
-	//{
-	//	// 1. 현재 바라보는 방향을 기준으로, 오른쪽으로 갈 수 있는지 확인
-	//	//int32 newDir = (_dir - 1) % DIR_COUNT;	// = ?
-	//	//if (CanGo(pos + front[newDir]))
-	//	//{
-	//	//	// 오른쪽 방향으로 90도 회전
+	while (pos != dest)
+	{
+		// 1. 현재 바라보는 방향을 기준으로, 오른쪽으로 갈 수 있는지 확인
+		int32 newDir = (_dir - 1) % DIR_COUNT;
+		if (CanGo(pos + front[newDir]))
+		{
+			// 오른쪽 방향으로 90도 회전
+			_dir = newDir;
 
-	//	//	// 앞으로 한 보 전진
+			// 앞으로 한 보 전진
+			pos = pos + front[_dir];
+			// 좌표 기록
+			_path.push_back(pos);
+		}
 
-	//	//	// 좌표 기록
-	//	//	_path.push_back(pos);
-	//	//}
+		// 2. 현재 바라보는 방향을 기준으로, 전진할 수 있는지 확인
+		else if (CanGo(pos + front[_dir]))
+		{
+			// 앞으로 한 보 전진
+			pos = pos + front[_dir];
+			// 좌표
+			_path.push_back(pos);
+		}
 
-	//	//// 2. 현재 바라보는 방향을 기준으로, 전진할 수 있는지 확인
-	//	//else if (CanGo(? ? ))
-	//	//{
-	//	//	// 앞으로 한 보 전진
-
-	//	//	// 좌표
-	//	//	_path.push_back(pos);
-	//	//}
-
-	//	//// 둘 다 안 된다면
-	//	//else
-	//	//{
-	//	//	// 왼쪽 방향으로 90도 회전
-	//	//}
-
-	//}
+		// 둘 다 안 된다면
+		else
+		{
+			// 왼쪽 방향으로 90도 회전
+			_dir = (_dir + 1) % DIR_COUNT;
+		}
+	}
 }
 
 void Player::CalculatePath_BFS()
