ea243ae8a0e554b18c121939f73873582e1ce14b
semin1012 <semin1012@gmail.com>
2024-05-19 20:19:54 +0900

    8주차 auto
    auto - 알아서 형식을 골라 주는 "형식 연역(type deduction)
    단점) 가독성이 좋지 않다
    장점) 이터레이터 같은 문법의 타이핑을 확 줄여 준다

--- GameCoding/GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding/GameCoding.cpp
@@ -3,60 +3,93 @@ using namespace std;
 #include <vector>
 #include <list>
 #include <queue>
+#include <map>
 #include "List.h"
+
+// auto ( c++11에서 나온 것, 새로운 문법 )
+
+class Knight
+{
+
+};
+
+template<typename T>
+void Print(T t)
+{
+	cout << t << endl;
+}
 
 int main()
 {
-	// size (resize)
-	// capacity (reserve) - X
-		// * list의 개념엔 capacity가 없음
-		// * 사이즈의 개념은 있지만 별도의 capacity는 없음
-	// 삽입/삭제?
-	// - 시작		O(1)
-	// - 중간		O(1) << 이 위치를 기억하고 있으면 O(1)이다.
-	// - 끝			O(1)
-	// front		O(1)
-	// back			O(1)
-	// push_front	O(1)
-	// push_back	O(1)
-	// 이론적으론 좋지만 노드 타고 가는 연산이 갉아먹기 때문에 벡터보다 느린 경우가 많다
-	// remove(value)
-	// 임의 접근 li[2]	X
+	// C++03
+	// C++11 (대격변, Modern C++)
+	// C++14
+	// C++17
+	// C++20 (2차 대격변, couritine, module)
+	// 우리가 배우는 것은 03, 그리고 이제 11이다.
 
-	list<int> li;
-
-	li.insert(li.end(), 1);
-	li.insert(li.end(), 2);
-	list<int>::iterator it = li.insert(li.end(), 3);
-	li.insert(li.end(), 4);
-	li.insert(li.end(), 5);
-	// insert - 원하는 위치에 넣을 수 있다. 이터레이터임
-	li.remove(5);
-	li.erase(it);
-	// erase - 원하는 위치를 삭제할 수 있다. 이터레이터임
-
-	// 순회, 벡터와 같다 
-	for (list<int>::iterator it = li.begin(); it != li.end(); )
 	{
-		int value = *it;
-		if (value % 2 == 0)
-			it = li.erase(it);
-		else it++;
+		int a = 3;
+		float b = 3.14f;
+		double c = 1.23;
+		Knight* d = new Knight();
+		const char* e = "semin";
+	}
+
+	{
+		auto a = 3;
+		auto b = 3.14f;
+		auto c = 1.23;
+		auto d = new Knight();
+		auto e = "semin";
+
+		// 이렇게 돌려도 오류 없다
+		// auto는 일종의 조커 카드 ( template과 뿌리가 비슷 )
+		Print<int>(1);
+		Print<const char*>("semin");
+		// 알아서 형식 골라 주는 것을 "형식 연역(type deduction)"이라고 한다.
+		// -> 말이 되게 잘 맞춰 주는 추론을 해 줌
+		// 주의: 추론 규칙이 생각보다 복잡해질 때가 있다.
+
+		// int* ptr = a;
+		// 이건 안 됨 말이 안 되는 문법임
+		int& ret = a;
+		const int cst = a;
+		
+		// 주의! auto는 const, &는 빼고 추론한다
+		auto& ret2 = ret;
+		const auto cst2 = cst;
+
+		vector<int> v{ 1,2,3,4,5 };
+
+		for (int i = 0; i < v.size(); i++)
+		{
+			// int& value = v[i];
+			// 원본을 건들려면 &를 붙여라
+			auto& value = v[i];
+			// auto를 쓸 때도 &를 붙여야 한다
+			if (value < 5)
+			{
+
+			}
+			value = 10;
+		}	
+
 	}
 	
-	//if (it != li.end())
-	//{
-	//	// 끝까지 다 왔다면 그것에 대한 처리
-	//}
+	// auto 의 문제점: 가독성
+	// - c++의 장점이 자료형 가독성이 좋다는 것인데
+	//	 가독성이 좋지 않다. auto를 막 쓰는 것은 c++ 장점을 망침.
+	//	 속도에 대해 치명적 단점은 없음. 가독성이 치명적. 무지성 사용 지양.
+	// auto 의 장점은?: 타이핑
+	// - 템플릿과 엮어 사용할 때 효과적일 수 있음.
+	// - vector<int>::iterator와 같은 귀찮은 타이핑을 줄일 수 있다
+	//   auto it = v.begin() 이런 건 누가 봐도 이터레이터인 거 알 수 있음
+	
+	map<int, int> m;
+	//std::map<int, int>::iterator it = m.begin();
+	auto it = m.begin();	// 위의 것을 auto로 사용하면 간단
 
-	List<int> myLi;
-	myLi.AddAtHead(10);
-	myLi.AddAtHead(20);
-	myLi.AddAtHead(30);
-
-	for (List<int>::Iterator myit = myLi.begin(); myit != myLi.end(); myit++)
-	{
-		int value = *myit;
-		cout << value << endl;
-	}
+	// 결론!) 앞으로 auto는  iterator의 경우메나 사용하기로 한다.
+	// 주의!) 복사면 안 되고 참조일 때 &를 붙이는 것은 잊지 말기.
 }
