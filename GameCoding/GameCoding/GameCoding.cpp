a8cab24b017a2c43c41ca3bd7f2a47646c5b5f29
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-05-26 23:10:00 +0900

    9주차 STL 알고리즘

--- GameCoding/GameCoding/GameCoding.cpp
+++ GameCoding/GameCoding/GameCoding.cpp
@@ -4,117 +4,178 @@ using namespace std;
 #include <list>
 #include <queue>
 #include <map>
-#include <set>
+#include <unordered_map>
+#include <algorithm>
 
-// map이랑 set이랑 짝꿍이다.
-// 키값만 있냐, 키와 밸류가 묶여 있냐 차이다.
-// 보통 키와 밸류가 묶여 있기 때문에 map으로 먼저 공부한다. 
+// find, find_if 자주 쓴다.
+// count, count_if 자주 쓴다.
+// all_of, any_of, none_of 종종 쓴다.
+// for_each 많이 쓴다.
+// remove, remove_if도 꽤 쓴다.
 
-class Player
+namespace Rookiss
 {
-public:
-	Player() { }
-	Player(int id) : _id(id) {} 
+	void Test()
+	{
 
-public:
-	int _id = 0;
-};
-
-// 만약 Player가 들고 있는 변수의 규모가 굉장히 커진다면,
-// Q) vector에 모든 Player 정보를 담는 것이 합당한가? vector로 관리해도 되는가?
-// -> 그래서 실제로는 참조로 들고 있는 것처럼 
-//	  vector<Player*> 이렇게 동적할당으로 들고 있는 경우가 많을 것이다.
-//	  이렇게 만들게 되면 성능은 좋아지는 대신 소멸까지 책임져야 한다. 
-
-template<typename T, typename U>
-struct Pair
-{
-	T first;
-	U second;
-};
-
-template<typename T, typename U>
-void MakePair(T first, U second)
-{
-	return std::pair<T, U>(first, second);
+	}
 }
+
+
 
 int main()
 {
-	vector<Player*> v;
-	v.push_back(new Player(100));
-	v.push_back(new Player(200));
-	v.push_back(new Player(300));
-	v.push_back(new Player(400));
-	v.push_back(new Player(500));
+	Rookiss::Test();
+	// 이름은 namespace라서 ::을 쓰게 되는 것이다. 
 
-	// vector로 관리하면 중간 삽입/삭제 같은 것에 한계가 있다. 
-	// map으로 관리하면 균형도 잘 맞을 뿐더러 왼쪽으로 가면 작고, 오른쪽으로 가면 크다.
-	// 업앤다운 놀이도 가능한 만능형 자료구조가 생긴 것이다. 
-	
-	// (key, value) 형식으로 저장 가능한 것이 map이다.
-	map<int, Player*> m;
-	
-	// 추가
-	// 찾기
-	// 삭제
-	// 순회
+	vector<int> v;
 
-	// 추가?
-	Pair<int, Player*> p;
-	int key = p.first;
-	Player* value = p.second;
-
-	// 벡터로 있는 정보를 추가한다면
-	for (Player* player : v)
+	for (int i = 0; i < 100; i++)
 	{
-		int key = player->_id;
-		Player* data = player;
-		
-		//m.insert(pair<int, Player*>(key, value));
-		// 만약 위의 코드가 귀찮다면?
-
-		MakePair(key, value); // = MakePair<int, Player*>(key, value);
-		// auto랑 template은 비슷한 느낌이다.
-		// auto, template은 컴파일러가 유추를 해줘서 형식 생략 가능하다. 
-		// 이 코드를 공식에서도 지원을 한다.
-		m.insert(make_pair(key, value));
-		// make_pair가 공식에서 지원하는 함수.
+		int n = rand() % 100;
+		v.push_back(n);
 	}
 
-	for (Player* player : v)
+	// Q) 특정 숫자가 있는지?
 	{
-		// 결국 간단하게 추가하고 싶으면 이렇게 쓰면 됨. 
-		// 위의 for문과 완전히 동일
-		m.insert(make_pair(key, value));
-		
-		//m[player->_id] = player;
-		// 위처럼 넣어도 되긴 하는데 덮어쓰는 문제랑 자동적 만들어지는 문제에 엮여있어서
-		// 나중에 언급하기로 하고 첫 번째 방식만 언급하기로 한다. 
+		int number = 50;
+
+		bool found = false;
+		vector<int>::iterator it;
+
+		for (auto it = v.begin(); it != v.end(); it++)
+		{
+			int value = *it;
+			if (value == number)
+			{
+				found = true;
+				break;
+			}
+		}
+		// 이렇게 해도 되고 현업에서도 쓰이는 코드지만
+		// 길이가 너무 길고 다 읽어야만 기능을 눈치 챌 수가 있다.
+
+		auto it = std::find(v.begin(), v.end(), number);
+		if (it == v.end())
+		{
+			cout << "못 찾음" << endl;
+		}
+		else
+		{
+			cout << "찾음" << endl;
+		}
+		// 성능은 완전히 똑같은데 가독성이 더 좋아진다.
+		// 순전히 가독성에만 이점이 있는 것이다. 
+		// 너무 특이한 함수는 쓰지 않는 게 좋다.
 	}
 
-	
-	// 순회, vector는 순차적으로 찾는 형식이었음
-	// map은 왼쪽으로 가냐, 오른쪽으로 가냐 이진탐색으로 효율적 탐색 가능.
-	auto it = m.find(300);	// iterator를 뱉는다.
-	
-	// 사용법 1
-	auto whoami = *it;
-	whoami.first;
-	whoami.second;
-	
-	// 사용법 2
-	it = m.find(300);	// O(logN), 이진탐색과 동일하다. 
-	if (it != m.end())
+	// Q2) 11로 나뉘는 숫자가 있는지 찾고 싶다면?
 	{
-		int key = it->first;
-		Player* value = it->second;
+		int div = 11;
+
+		vector<int>::iterator it;
+
+		for (it = v.begin(); it != v.end(); it++)
+		{
+			int value = *it;
+			if (value % div == 0)
+			{
+				break;
+			}
+		}
+		// 안에 것만 바뀜
+
+		struct CanDivideBy11	// 나중에 람다를 배우면 람다로 하면 된다.
+		{
+			bool operator()(int n)
+			{
+				return n % 11 == 0;
+			}
+		};
+
+		auto it = std::find_if(v.begin(), v.end(), CanDivideBy11());
+		if (it == v.end())
+		{
+			cout << "못 찾음" << endl;
+		}
+		else
+		{
+			cout << "찾음" << endl;
+		}
 	}
-	else
+
+	// Q) 홀수인 숫자 개수는?
 	{
-		cout << "없음" << endl;
+		int count = 0;
+		for (auto it = v.begin(); it != v.end(); it++)
+		{
+			if (*it % 2 != 0)
+				count++;
+		}
+
+		struct IsOdd
+		{
+			bool operator()(int n)
+			{
+				return n % 2 != 0;
+			}
+		};
+
+		int n = std::count_if(v.begin(), v.end(), IsOdd());
+
+		// 모든 데이터가 홀수입니까?
+		bool b1 = std::all_of(v.begin(), v.end(), IsOdd());
+		// 홀수인 데이터가 하나라도 있습니까?
+		bool b1 = std::any_of(v.begin(), v.end(), IsOdd());
+		// 모든 데이터가 홀수가 아닙니까?
+		bool b1 = std::none_of(v.begin(), v.end(), IsOdd());
 	}
 
+	// Q) 벡터에 있는 모든 숫자들에 3을 곱해 주세요
+	{
+		for (int i = 0; i < v.size(); i++)
+		{
+			v[i] *= 3;
+		}
 
+		struct MultiplyBy3
+		{
+			void operator()(int& n)
+			{
+				n *= 3;
+			}
+		};
 
+		std::for_each(v.begin(), v.end(), MultiplyBy3());
+		// 상대적으로 활용 빈도가 높다. 기왕이면 만들어진 것을 활용하는 게 좋으니 사용.
+	}
+
+	// Q) 홀수인 데이터를 일괄 삭제
+	{
+		vector<int> v = { 1, 4, 3, 5, 8, 2 };
+
+		/*for (auto it = v.begin(); it != v.end(); )
+		{
+			if (*it % 2 != 0)
+				it = v.erase(it);
+			else
+				it++;
+		}*/
+
+		struct IsOdd
+		{
+			bool operator()(int n)
+			{
+				return n % 2 != 0;
+			}
+		};
+
+		// 1 4 3 5 8 2 여기에서 홀수 삭제하기
+		vector<int>::iterator it = std::remove_if(v.begin(), v.end(), IsOdd());
+		// 이거 돌리면 4 8 2 5 8 2 < 이렇게 된다
+		// iterator가 5를 가리키고 있음. 그래서 뒷부분 삭제하라고 하는 거.
+		v.erase(it, v.end());
+		// 삭제해줘야 한다.
+		// remove_if 사용할 때는 꼭 주의해야 한다.
+	}
 }
