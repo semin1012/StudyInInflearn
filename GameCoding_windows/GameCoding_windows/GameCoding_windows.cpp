8a8f7972a87b222bd64ccbf822f1665f762481c5
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-06-03 22:09:54 +0900

    10주차 winAPI 기본 템플릿 분석
    - 마우스 무브 시에 InvalidateRect를 호출해서 강제로 다시 화면을 그리는 방식이다.
    - 이렇게 그려서는 실시간으로 계속 렌더링하는 게임에는 적합하지 않다.

---
+++ GameCoding_windows/GameCoding_windows/GameCoding_windows.cpp
@@ -0,0 +1,193 @@
+﻿// GameCoding_windows.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "framework.h"
+#include "GameCoding_windows.h"
+
+#define MAX_LOADSTRING 100
+
+int mousePosX;
+int mousePosY;
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    // TODO: 여기에 코드를 입력합니다.
+
+    // 전역 문자열을 초기화합니다.
+    // :: 붙여도 돌아가면 프로젝트에 종속적인 코드가 아니라 표준 제공 함수다. 
+
+    // 1) 윈도우 창 정보 등록
+    MyRegisterClass(hInstance);
+
+
+    // 2) 윈도우 창 생성
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    MSG msg;
+
+    // 3) 메인 루프
+
+    // - 입력
+    // - 로직
+    // - 렌더링
+
+    while (::GetMessage(&msg, nullptr, 0, 0))   // 메시지가 있으면 처리해준다
+    {
+        ::TranslateMessage(&msg);
+        ::DispatchMessage(&msg);    // 메시지 처리하는 부분 
+     }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  용도: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;  // 함수 포인터, 이벤트 처리를 이걸로 한다.
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GAMECODINGWINDOWS));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_GAMECODINGWINDOWS);  
+    wcex.lpszClassName = L"GameCodingWindows"; //szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+    // 어떤 정보를 입력을 해서 등록하는 함수
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   주석:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   RECT windowRect = { 0, 0, 800, 600 };
+   ::AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, false);
+
+   /* 중요한 함수는 CreateWindowW */
+   HWND hWnd = CreateWindowW(L"GameCodingWindows", L"Client", WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, windowRect.right - windowRect.left, windowRect.bottom - windowRect.top, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ::ShowWindow(hWnd, nCmdShow);
+   ::UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+
+// 메시지를 어떻게 처리할지 CallBack
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다:
+            switch (wmId)
+            {
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        // 그리는 것과 관련됨
+        // 다시 그려야 할 필요가 있을 때만 다시 그린다. 그래서 게임 같은 경우에 이런 기본 골격으로 만들 수 없다.
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+
+            // 문자
+            // 우리가 관리를 하지만 메모리 어딘가에 들고 있고 윈도우가 책임짐
+            WCHAR buffer[100];
+            ::wsprintf(buffer, L"(%d, %d)", mousePosX, mousePosY);
+            // 문자형식을 만든 다음 buffer에 넣어 준다.
+            ::TextOut(hdc, 100, 100, buffer, ::wcslen(buffer));
+
+            // 사각형
+            ::Rectangle(hdc, 200, 200, 400, 400);
+
+            // 원
+            ::Ellipse(hdc, 200, 200, 400, 400);
+
+            // 선
+            ::MoveToEx(hdc, 300, 300, nullptr);
+            ::LineTo(hdc, 400, 400);
+            ::LineTo(hdc, 500, 300);
+
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_MOUSEMOVE:
+        // lParam & 0xFFFF;
+        // lParam >> 16;
+        mousePosX = LOWORD(lParam);
+        mousePosY = HIWORD(lParam);
+        ::InvalidateRect(hWnd, nullptr, TRUE);  // 이전 내용 지우고 다시 그리라는 것
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
