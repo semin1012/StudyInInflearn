ec3246a7493626c9bd031b17d0aa37d625bad5ff
semin1012 <73764069+semin1012@users.noreply.github.com>
2024-06-06 16:43:52 +0900

    10주차 더블 버퍼링

--- GameCoding_windows/GameCoding_windows/Game.cpp
+++ GameCoding_windows/GameCoding_windows/Game.cpp
@@ -24,6 +24,18 @@ void Game::Init(HWND hwnd)
 	_hwnd = hwnd;			// 윈도우 번호
 	_hdc = ::GetDC(hwnd);	// 도화지 번호
 
+	::GetClientRect(hwnd, &_rect);
+
+
+	// 더블 버퍼링에 대해서 면접에서 묻는 경우 있다. 알아둘 것.
+	_hdcBack = ::CreateCompatibleDC(_hdc);		// _hdc와 호환되는 DC를 생성
+	_bmpBack = ::CreateCompatibleBitmap(_hdc, _rect.right, _rect.bottom);	// _hdc와 호환되는 비트맵 생성
+	HBITMAP prev = (HBITMAP)::SelectObject(_hdcBack, _bmpBack);	// DC와 BMP를 연결
+	::DeleteObject(prev);
+	// 도화지 -> 비트맵으로 연결 -> 이전 것은 필요 없으니 날려준다, 더블 버퍼링
+
+
+
 	//TimeManager::GetInstance()->Init();
 	GET_SINGLE(TimeManager)->Init();
 	GET_SINGLE(InputManager)->Init(hwnd);
@@ -43,14 +55,30 @@ void Game::Render()
 {
 	uint32 fps = GET_SINGLE(TimeManager)->GetFps();
 	float deltaTime = GET_SINGLE(TimeManager)->GetDeltaTime();
-
-	GET_SINGLE(SceneManager)->Render(_hdc);
 	
+	{
+		POINT mousePos = GET_SINGLE(InputManager)->GetMousePos();
+		wstring str = std::format(L"Mouse({0}, {1})", mousePos.x, mousePos.y);
+		::TextOutW(_hdcBack, 20, 10, str.c_str(), static_cast<int32>(str.size()));
+	}
+
+
 	{
 		// WCHAR  buffer[100]; // 이건 옛날 방식이다.
 		wstring str = std::format(L"FPS({0}), DT({1} ms)", fps, static_cast<int32>(deltaTime * 1000));
-		::TextOutW(_hdc, 650, 10, str.c_str(), static_cast<int32>(str.size()));
+		::TextOutW(_hdcBack, 650, 10, str.c_str(), static_cast<int32>(str.size()));
 	}
-		
-	// 더블 버퍼링에 대해서 면접에서 묻는 경우 있다. 알아둘 것.
+	
+	GET_SINGLE(SceneManager)->Render(_hdcBack);
+
+	// Double Buffering
+	::BitBlt(_hdc, 0, 0, _rect.right, _rect.bottom, _hdcBack, 0, 0, SRCCOPY);	
+	// 비트블릿: 고속 복사, 메모리 DC의 비트맵을 현재 DC에 고속복사하는 함수
+	// _hdcBack 내용을 hdc에 복사
+	::PatBlt(_hdcBack, 0, 0, _rect.right, _rect.bottom, WHITENESS);
+	// 패트블릿: 초기화하는 것. 화이트로 초기화를 했다.
+	// 패트블릿으로 초기화를 하고 비트블릿으로 백버퍼에 저장.
+
+	// 더블 버퍼링은 출력하는 것과 지우는 것을 따로 분리한 것이다. 
+	// 하나는 출력만 하고, 하나는 지우기만 한다. 
 }
--- GameCoding_windows/GameCoding_windows/Game.h
+++ GameCoding_windows/GameCoding_windows/Game.h
@@ -16,5 +16,11 @@ public:
 private:
 	HWND _hwnd = {};
 	HDC _hdc = {};
+
+private:
+	// Double Buffering
+	RECT _rect;
+	HDC _hdcBack = {};
+	HBITMAP _bmpBack = {};
 };
 
